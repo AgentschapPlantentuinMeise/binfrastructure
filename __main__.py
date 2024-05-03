47737b545be99513fd2428afec5f3c0ad4c1255d
Christophe Van Neste <christophe.vanneste@plantentuinmeise.be>
2024-05-03 16:15:08 +0200

    now with web entry

--- __main__.py
+++ __main__.py
@@ -4,14 +4,34 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('b3-bucket')
-
+bucket = s3.Bucket('b3-bucket',
+    website=s3.BucketWebsiteArgs(
+        index_document="index.html",
+    ),
+)
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
 
 # Create an S3 Bucket object
-bucketObject = s3.BucketObject(
+ownership_controls = s3.BucketOwnershipControls(
+    'ownership-controls',
+    bucket=bucket.id,
+    rule=s3.BucketOwnershipControlsRuleArgs(
+        object_ownership='ObjectWriter',
+    ),
+)
+
+public_access_block = s3.BucketPublicAccessBlock(
+    'public-access-block', bucket=bucket.id, block_public_acls=False
+)
+
+bucket_object = s3.BucketObject(
     'index.html',
     bucket=bucket.id,
-    source=pulumi.FileAsset('./index.html')
+    source=pulumi.FileAsset('index.html'),
+    content_type='text/html',
+    acl='public-read',
+    opts=pulumi.ResourceOptions(depends_on=[public_access_block, ownership_controls]),
 )
+
+pulumi.export('bucket_endpoint', pulumi.Output.concat('http://', bucket.website_endpoint))
